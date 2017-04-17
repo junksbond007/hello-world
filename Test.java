3e5a1ec5274f0a270be279e334c8646a88f2c4ae
junksbond007 <junksbond007@gmail.com>
2017-04-17 12:27:28 -0500

    adding my name console statement
    adding my name console statement

--- Test.java
+++ Test.java
@@ -1,4 +1,8 @@
 public static void main(String[] args) {
   Sysout.out.println("Hello-World");
   Sysout.out.println("Hello-GITHub");
+  
+  String name = "zeyaul";
+  
+  System.out.println("My name is = " + name);
 }
